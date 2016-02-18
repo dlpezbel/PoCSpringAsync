69a065627238be8af24f1ac83064d14e3cb40d82
dlpezbel <dlpezbel@yahoo.es>
2016-02-18 17:35:49 +0100

    .

--- PoCSpringAsync/src/main/java/poc/springasync/Validator.java
+++ PoCSpringAsync/src/main/java/poc/springasync/Validator.java
@@ -5,13 +5,13 @@ import java.util.concurrent.Future;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
+
 
 @Component
 public class Validator {
 
     @Async
-    public Future<String> findUser(Integer i) throws InterruptedException {
+    public Future<String> validate(Integer i) throws InterruptedException {
         System.out.println("Validation started ...  " + i);
         //User results = restTemplate.getForObject("https://api.github.com/users/" + user, User.class);
         // Artificial delay of 1s for demonstration purposes
